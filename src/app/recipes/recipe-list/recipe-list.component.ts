858e05eedd2df73e887b6747e74313e757c0faf7
aeherrer71 <aeherrer71@gmail.com>
2023-03-09 21:12:01 -0500

    Outputting dynamic data with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
+    new Recipe('This is a test Recipe', 'Test Recipe Description', 'https://www.dominicancooking.com/wp-content/uploads/how-to-make-sancocho-step-by-step-6.jpg' ),
+    new Recipe('This is a test Recipe', 'Test Recipe Description', 'https://www.dominicancooking.com/wp-content/uploads/how-to-make-sancocho-step-by-step-6.jpg' ),
+    new Recipe('This is a test Recipe', 'Test Recipe Description', 'https://www.dominicancooking.com/wp-content/uploads/how-to-make-sancocho-step-by-step-6.jpg' ),
     new Recipe('This is a test Recipe', 'Test Recipe Description', 'https://www.dominicancooking.com/wp-content/uploads/how-to-make-sancocho-step-by-step-6.jpg' )
   ];
   ngOnInit() {
